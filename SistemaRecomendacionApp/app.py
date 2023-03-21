591f485d60740fc32cb97ef718cd4440be658500
rgramos <riki_german@yahoo.com.ar>
2023-03-20 22:07:35 -0300

    proyecto recomendaciones actualizado

--- SistemaRecomendacionApp/app.py
+++ SistemaRecomendacionApp/app.py
@@ -4,15 +4,38 @@ import pickle
 
 grid_search = pickle.load(open('grid_search_svd.sav','rb'))
 
-def greet(usuario, peli):
-	res = grid_search.predict(usuario, peli).est
-	return round(res,2)
+def recomendation_system(usuario, pelicula):
+	estimated_rating = grid_search.predict(usuario, pelicula).est
+	return round(estimated_rating,2)
 
 
-demo = gr.Interface(
-	fn=greet, 
-	inputs=['text', 'text'],
-	outputs="number"
-	)
+# demo = gr.Interface(
+# 	recomendation_system,
+# 	[
+# 		'text',
+# 		'text'
+# 	],
+# 	"number",
+# 	examples=[['124380','as5250']],
+# 	title='Sistema de recomendaciones',
+# 	description='Prediga el puntaje de una película por un usuario del catálogo de Amazon',
+# )
+
+
+
+with gr.Blocks(theme=gr.themes.Monochrome()) as demo:
+	gr.Markdown(
+        """
+	    # Sistema de recomendaciones
+	    Prediga el puntaje de una película por un usuario del catálogo de **Amazon**.
+	    """
+    )
+	usuario = gr.Textbox(label="Id Usuario")
+	pelicula = gr.Textbox(label="Id Pelicula")
+	output = gr.Textbox(label="Puntaje estimado")
+	predict_btn = gr.Button("Predecir puntaje")
+	predict_btn.click(fn=recomendation_system, inputs=[usuario,pelicula], outputs=output)
+
 
 demo.launch()
+
